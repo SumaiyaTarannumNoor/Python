165c934b5756c73ad666e11eadf13657a363b0d9
Sumaiya Tarannum Noor <sumaiyatarannumnoor@gmail.com>
2022-02-15 22:24:44 +0600

    Add files via upload

---
+++ 6. Swap two variables.py
@@ -0,0 +1,17 @@
+
+#x = 5
+#y = 10
+
+x = int(input('Enter a number: '))
+y = int(input('Enter a number: '))
+        
+
+temp = x
+x = y
+y = temp
+
+#print(x)
+#print(y)
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
