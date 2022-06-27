55ced9b78f782144c37fa566d23d319f0744e06f
Sumaiya Tarannum Noor <sumaiyatarannumnoor@gmail.com>
2022-06-27 10:08:47 +0600

    Update 23. Madlib (12 small Python Projects).py

--- 23. Madlib (12 small Python Projects).py
+++ 23. Madlib (12 small Python Projects).py
@@ -1,15 +1,11 @@
-# youtuber= 'Sumaiya'
-
-# print('subscribe to' + youtuber)
-# print('subscribe tp {}'.format(youtuber))
-# print(f'subscribe to {youtuber}')
-
 adj = input("Adjective: ")
 verb1 = input("Verb: ")
 verb2 = input("Verb: ")
+verb3 = input("Verb: ")
+verb4 = input("Verb: ")
 famous_person = input("Famous Person: ")
 
-madlib = f"He is so {adj}! I {verb1} him so  much.\
-    I want to {verb2} him. He is my {famous_person}"
+madlib = f"It is {adj}! They {verb1} with anyone they see and {verb3}.\
+    It should be {verb2} and done line {verb1}. We are {famous_person}. Because we {verb4}."
 
 print(madlib)
