4e051a68735bdfa6e8b60d549037a8fd098adbc7
Sumaiya Tarannum Noor <sumaiyatarannumnoor@gmail.com>
2024-03-07 11:06:59 +0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,41 +1,41 @@
-@app.route('/registration', methods=['GET', 'POST'])
-def registration():
-    try:
-        if request.method == 'POST':
-            full_name = request.form.get('full_name')
-            email = request.form.get('email')
-            organization = request.form.get('organization')
-            phone_number = request.form.get('phone_number')
-            address = request.form.get('address')
-            educational_level = request.form.get('educational_level')
-            skills = request.form.get('skills')
-            freelancing_experience = request.form.get('freelancing_experience')
-            portfolio_link = request.form.get('portfolio_link')
-            language_proficiency = request.form.get('language_proficiency')
-            # institution = request.form.get('institution')
-            # educational_certificates = request.form.get('educational_certificates
-            # availability = request.form.get('availability')
-            # preferred_work_type = request.form.get('preferred_work_type')
-            # hourly_rate = request.form.get('hourly_rate')
-            # certifications = request.form.get('certifications')
-            # linkedIn_profile = request.form.get('linkedIn_profile')
-            # github_profile = request.form.get('github_profile')
-            # course_joining_date = request.form.get('course_joining_date')
+@app.route('/trainee_list_edit/<int:trainee_id>', methods=['GET','POST'])
+def trainee_list_edit(trainee_id):
+    if request.method == 'POST':
+        trainee_id = request.form.get('trainee_id')
+        full_name = request.form.get('full_name')
+        email = request.form.get('email')
+        organization = request.form.get('organization')
+        phone_number = request.form.get('phone_number')
+        address = request.form.get('address')
+        educational_level = request.form.get('educational_level')
+        skills = request.form.get('skills')
+        freelancing_experience = request.form.get('freelancing_experience')
+        portfolio_link = request.form.get('portfolio_link')
+        language_proficiency = request.form.get('language_proficiency')
 
-        # Check for required fields
-            if not ([full_name, email, phone_number, address, educational_level]):
-                return jsonify({"message": "You must fill up these required fields."}), 400
-            
-            # Perform database operations
+        # Perform database operations
+        try:
             with connection.cursor() as cursor:
-                trainee_create_sql = "INSERT INTO trainees (full_name, organization, email, phone_number, address, educational_level, skills, freelancing_experience, portfolio_link, language_proficiency) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                cursor.execute(trainee_create_sql, (full_name, organization, email, phone_number, address, educational_level, skills, freelancing_experience, portfolio_link,language_proficiency))
+                trainee_update_sql = "UPDATE trainees SET full_name=%s, organization=%s, email=%s, phone_number=%s, address=%s, educational_level=%s, skills=%s, freelancing_experience=%s, portfolio_link=%s, language_proficiency=%s WHERE trainee_id = %s"
+                cursor.execute(trainee_update_sql, (full_name, organization, email, phone_number, address, educational_level, skills, freelancing_experience, portfolio_link, language_proficiency, trainee_id))
                 connection.commit()
 
-            return jsonify({'success': 'Registration successful'}), 200
-        
-        elif request.method == 'GET':
-            return jsonify({'message': 'GET request received'}), 200
+            return jsonify({'success': 'Trainee info updated successfully'}), 200
+        except Exception as e:
+            return jsonify({'error': f"Request error: {str(e)}"}), 500
 
+
+@app.route('/trainee_delete/<int:trainee_id>', methods=['GET','POST'])
+def trainee_delete(trainee_id):
+    try:
+        if request.method == 'POST':
+            # trainee_id = request.form.get('trainee_id ')
+            # print(trainee_id )
+            cursor = connection.cursor()
+            event_delete_query = "DELETE FROM trainees  WHERE trainee_id  = %s"
+            cursor.execute(event_delete_query, (trainee_id,))
+            connection.commit()
+            return jsonify({'success': 'Delete Success'})
+        return jsonify({'error': 'Invalid request'})
     except Exception as e:
-        return jsonify({'error': f"Request error: {str(e)}"}), 500
+        return jsonify({'error': f"Request error: {str(e)}"})
