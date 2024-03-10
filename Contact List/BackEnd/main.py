56c6cc8e79e7f8470db26d5611070bdf7aa0bcf1
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-03-10 16:44:37 +0600

    Add files via upload

--- Contact List/BackEnd/main.py
+++ Contact List/BackEnd/main.py
@@ -5,26 +5,26 @@ from models import Contact
 @app.route('/contacts', methods=['GET'])
 def get_contacts():
     contacts = Contact.query.all()
-    json_contacts = list(map(lambda x: x.to_json(), contacts))
+    json_contacts = [contact.to_json() for contact in contacts]
     return jsonify({"contacts": json_contacts}), 200
 
 @app.route('/create_contact', methods=['POST'])
 def create_contact():
-    first_name = request.json.get("firstName")
-    last_name = request.json.get("lastName")
-    email = request.json.get("email")
+    data = request.json
+    first_name = data.get("firstName")
+    last_name = data.get("lastName")
+    email = data.get("email")
 
-    if not first_name or last_name or not email:
-        return jsonify({"message": "You must fill up all the fields."}, 400)
+    if not first_name or not last_name or not email:
+        return jsonify({"message": "You must fill up all the fields."}), 400
 
     new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
     try:
         db.session.add(new_contact)
         db.session.commit()
+        return jsonify({"message": "New Contact is created!"}), 201
     except Exception as e:
-        return jsonify({"message": str(e)}), 400
-
-    return jsonify({"message": "New Contact is created!"}), 201
+        return jsonify({"message": str(e)}), 422
 
 @app.route('/update_contact/<int:contact_id>', methods=['PATCH'])
 def update_contact(contact_id):
@@ -38,17 +38,18 @@ def update_contact(contact_id):
     contact.last_name = data.get("lastName", contact.last_name)
     contact.email = data.get("email", contact.email)
 
-    db.session.commit()
+    try:
+        db.session.commit()
+        return jsonify({"message": "Contact Updated."}), 200
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
 
-    return jsonify({"message": "Contact Updated."}), 201
-
-
-@app.route('/delete_contact/<int:user_id>', methods=['DELETE'])
-def delete_contact(user_id):
-    contact = Contact.query.get(user_id)
+@app.route('/delete_contact/<int:contact_id>', methods=['DELETE'])
+def delete_contact(contact_id):
+    contact = Contact.query.get(contact_id)
 
     if not contact:
-        return jsonify({"message": "User not found"}), 404
+        return jsonify({"message": "Contact not found"}), 404
 
     db.session.delete(contact)
     db.session.commit()
