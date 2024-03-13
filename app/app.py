42334badf6d934f3c068c5ebab1c41986a061478
Sumaiya Tarannum Noor <sumaiyatarannumnoor@gmail.com>
2024-03-13 13:00:50 +0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,78 @@
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    try:
+        if request.method == 'POST':
+            full_name = request.form.get('full_name')
+            email = request.form.get('email')
+            organization = request.form.get('organization')
+            phone_number = request.form.get('phone_number')
+            address = request.form.get('address')
+            educational_level = request.form.get('educational_level')
+            skills = request.form.get('skills')
+            freelancing_experience = request.form.get('freelancing_experience')
+            # portfolio_link = request.form.get('portfolio_link')
+            # language_proficiency = request.form.get('language_proficiency')
+            # institution = request.form.get('institution')
+            # educational_certificates = request.form.get('educational_certificates
+            # availability = request.form.get('availability')
+            # preferred_work_type = request.form.get('preferred_work_type')
+            # hourly_rate = request.form.get('hourly_rate')
+            # certifications = request.form.get('certifications')
+            # linkedIn_profile = request.form.get('linkedIn_profile')
+            # github_profile = request.form.get('github_profile')
+            # course_joining_date = request.form.get('course_joining_date')
+            # Handling language proficiency
+            portfolio_links = {}
+
+            # Extract portfolio links for selected platforms
+            if 'fiverr' in request.form:
+                portfolio_links['fiverr'] = request.form.get('fiverr_link')
+            if 'upwork' in request.form:
+                portfolio_links['upwork'] = request.form.get('upwork_link')
+            if 'freelancer' in request.form:
+                portfolio_links['freelancer'] = request.form.get('freelancer_link')
+            if 'toptal' in request.form:
+                portfolio_links['toptal'] = request.form.get('toptal_link')
+            if 'guru' in request.form:
+                portfolio_links['guru'] = request.form.get('guru_link')
+            if 'other' in request.form:
+                portfolio_links['other'] = request.form.get('other_link')
+
+            # Convert the dictionary to a JSON string
+            json_data = json.dumps(portfolio_links)
+        
+
+            languages = []
+            if 'bangla' in request.form:
+                languages.append('Bangla')
+            if 'english' in request.form:
+                languages.append('English')
+            if 'other' in request.form:
+                other_language = request.form.get('other_language')
+                if other_language:
+                    languages.append(other_language)
+            # print(languages)
+            # exit()
+            language_proficiency = ', '.join(languages)
+
+        # Check for required fields
+            if not ([full_name, email, phone_number, address, educational_level]):
+                return jsonify({"message": "You must fill up these required fields."}), 400
+            
+            # Perform database operations
+            with connection.cursor() as cursor:
+                trainee_create_sql = "INSERT INTO trainees (full_name, organization, email, phone_number, address, educational_level, skills, freelancing_experience, portfolio_link, language_proficiency) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                cursor.execute(trainee_create_sql, (full_name, organization, email, phone_number, address, educational_level, skills, freelancing_experience, json_data,language_proficiency))
+                connection.commit()
+
+            return jsonify({'success': 'Registration successful'}), 200
+        
+        elif request.method == 'GET':
+            return jsonify({'message': 'GET request received'}), 200
+
+    except Exception as e:
+        return jsonify({'error': f"Request error: {str(e)}"}), 500
+
 @app.route('/trainee_list_edit/<int:trainee_id>', methods=['GET','POST'])
 def trainee_list_edit(trainee_id):
     if request.method == 'POST':
