56768dda4a98427af81bf8e13fb009b0c766624c
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-06-05 17:11:41 +0600

    Add files via upload

---
+++ Python Turtle Arts/HexagonSpirograph.py
@@ -0,0 +1,19 @@
+# Original - https://www.youtube.com/watch?v=_JxgaQ4xT3E&list=PLS9qLR8VoFA56NWSswK2daQSovI9QCpQE&index=11
+
+from turtle import *
+
+speed(0)
+bgcolor("black")
+pensize(2)
+
+colours = ["blue", "magenta", "yellow", "hotpink", "pink", "skyblue", "orange", "red", "green", "cyan"]
+for i in range(360):
+   pencolor(colours[i%6])
+   width(i / 250 +1)
+   forward(i)
+   left(59)
+
+hideturtle()
+
+done()
+
