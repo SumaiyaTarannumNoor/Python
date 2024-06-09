3263094374a4ad383bd111298c62b9c91047905f
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-06-09 12:48:04 +0600

    Add files via upload

---
+++ Python Turtle Arts/BangladeshiFlag.py
@@ -0,0 +1,21 @@
+# Original - https://www.youtube.com/watch?v=RKzLLgU5IVo&list=PLS9qLR8VoFA56NWSswK2daQSovI9QCpQE&index=30
+
+from turtle import *
+
+speed(0)
+setup(800, 550)
+bgcolor("green")
+
+penup()
+goto(-80, -80)
+pendown()
+
+
+color("red")
+begin_fill()
+circle(120)
+end_fill()
+
+hideturtle()
+
+done()
