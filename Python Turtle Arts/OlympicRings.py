8ce68e926efbb3ba6f1a4f68dc5be93b304380cb
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2024-06-09 14:03:35 +0600

    Add files via upload

---
+++ Python Turtle Arts/OlympicRings.py
@@ -0,0 +1,37 @@
+from turtle import *
+
+speed(0)
+setup(800, 600)
+pensize(10)
+
+pencolor("black")
+circle(80)
+
+penup()
+goto(-200, 0)
+pendown()
+pencolor("blue")
+circle(80)
+
+penup()
+goto(200, 0)
+pendown()
+pencolor("red")
+circle(80)
+
+penup()
+goto(-100, -100)
+pendown()
+pencolor("yellow")
+circle(80)
+
+penup()
+goto(100, -100)
+pendown()
+pencolor("green")
+circle(80)
+
+hideturtle()
+
+done()
+
