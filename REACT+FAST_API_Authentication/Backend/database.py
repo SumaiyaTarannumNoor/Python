713f8827184104937a5e81f9461baf94c3f95973
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2025-02-17 13:25:20 +0600

    Add files via upload

---
+++ REACT+FAST_API_Authentication/Backend/database.py
@@ -0,0 +1,15 @@
+# database.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Fix: Changed SQLITE to sqlite in the URL
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
