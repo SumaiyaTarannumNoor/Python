5dadeb3fb4bdcf95209a6c7be9d978a778c7d234
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2025-07-17 22:38:48 +0600

    Add files via upload

---
+++ Web Scraping/wikiquote.py
@@ -0,0 +1,34 @@
+import random
+import requests
+from bs4 import BeautifulSoup
+
+
+USER_AGENTS = {
+    "chrome": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+    "firefox": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:117.0) Gecko/20100101 Firefox/117.0",
+    "edge": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
+}
+
+def get_headers():
+    return {'User-Agent': random.choice(list(USER_AGENTS.values()))}
+
+def scrape_wikiquotes():
+    url = "https://en.wikiquote.org/wiki/Proverbs"
+    response = requests.get(url, headers=get_headers())
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    quotes = []
+
+    for li in soup.select("div.mw-parser-output > ul > li"):
+        text = li.get_text(strip=True)
+        if len(text) >10 and not text.startswith("Retrieved"):
+            quotes.append(text) 
+
+    with open("wikiquotes.txt", "w", encoding='utf-8') as f:
+        for quote in quotes:
+            f.write(quote + '\n')   
+
+    print(f"Wikiquotes: {len(quotes)} saved in wikiquotes.txt.")
+
+scrape_wikiquotes()                 
