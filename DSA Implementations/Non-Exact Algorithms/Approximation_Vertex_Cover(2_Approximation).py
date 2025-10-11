8dcad61230e9ae96ba21d0eff1efd4addffff094
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2025-10-11 19:10:32 +0600

    Add files via upload

---
+++ DSA Implementations/Non-Exact Algorithms/Approximation_Vertex_Cover(2_Approximation).py
@@ -0,0 +1,21 @@
+def vertex_cover(graph):
+    cover = set()
+    edges = list(graph.keys())
+    while edges:
+        u, v = edges.pop()
+        cover.add(u)
+        edges = [(x, y) for (x, y) in edges if x not in cover and y not in cover]
+    return cover
+
+
+graph = {
+    (1, 2): 1,
+    (1, 3): 1,
+    (2, 3): 1,
+    (3, 4): 1,
+}
+
+print(vertex_cover(graph))
+
+# Time Complexity: O(V + E)
+# Space Complexity: O(V)
