3ef68b645549d23dd52f3963e2ab349e73a3cbed
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2026-01-23 01:29:52 +0600

    Mistral Integation using Ollama

---
+++ OLLAMA/API-FOR-LLM/test-api.py
@@ -0,0 +1,12 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+url="http://127.0.0.1:8000/generate?prompt=Hello"
+headers = {"x-api-key": os.getenv("API_KEY"), "Content-Type": "application/json"}
+
+response = requests.post(url, headers=headers)
+print(response.json())
+
